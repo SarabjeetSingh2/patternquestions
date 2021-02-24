bea2c117c83f1ec7f6d6b1c05f4c9c429cbcaf43
SarabjeetSingh2 <79589903+SarabjeetSingh2@users.noreply.github.com>
2021-02-24 22:16:48 +0530

    Add files via upload

---
+++ Q8.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j,k,f,l;
+    cin>>n;
+    f=(n+1)/2;
+    l=(n-1)/2;
+     for(i=0;i<f;i++)
+    {
+        for(j=0;j<f-i-1;j++)
+        {
+            cout<<" ";
+        }
+
+        for(k=0;k<2*i+1;k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    for(i=0;i<l;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            cout<<" ";
+        }
+
+        for(k=0;k<(l-i)*2-1;k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
